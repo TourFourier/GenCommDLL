81c11db0750332ba1e54e8fe46b0ea254fd45d79
TourFourier <43444695+TourFourier@users.noreply.github.com>
2019-04-07 15:52:42 +0300

    queue

--- GenCommDLL/CSafeMessageQueue.cpp
+++ GenCommDLL/CSafeMessageQueue.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "CSafeMessageQueue.h"
+#include "IMessage.h"
+#include <queue>
 
 
 CSafeMessageQueue::CSafeMessageQueue()
--- GenCommDLL/CSafeMessageQueue.h
+++ GenCommDLL/CSafeMessageQueue.h
@@ -8,8 +8,13 @@
 
 class DLL CSafeMessageQueue
 {
+	CRITICAL_SECTION m_cs;
+	std::queue<IMessage> m_qMessageQueue;
 public:
 	CSafeMessageQueue();
 	~CSafeMessageQueue();
+
+	void Push(IMessage message);
+	IMessage Pop();
 };
 
